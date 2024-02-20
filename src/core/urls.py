3b6a66084dc11fb58ea3f7aa18bc2750410450b9
mark-akbarov <markakbarov@gmail.com>
2024-02-20 12:08:47 +0500

    modify and add docker

--- src/core/urls.py
+++ src/core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from bot import webhook
+from bot.bot import webhook
 
 
 urlpatterns = [
