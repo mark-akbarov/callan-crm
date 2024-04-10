55dcd335c61dfd42e4dd2ce5ea10ad6931b88eda
Umarkhon Akbarov <markakbarov@gmail.com>
2024-04-10 06:27:17 +0000

    minor fix

--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@
 
 cd /home/mrk/callan-crm
 source .venv/bin/activate
+<<<<<<< HEAD
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
+=======
+exec python manage.py runserver 0.0.0.0:8000
+# exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
+>>>>>>> 2212e8b (minor fix)
