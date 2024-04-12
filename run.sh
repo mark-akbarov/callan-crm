a865bec465769e292e73f9799c9a72d70629e6bc
Umarkhon Akbarov <u.akbarov@uic.group>
2024-04-12 23:09:33 +0500

    Revert "minor fix"
    This reverts commit 55dcd335c61dfd42e4dd2ce5ea10ad6931b88eda.

--- run.sh
+++ run.sh
@@ -2,9 +2,4 @@
 
 cd /home/mrk/callan-crm
 source .venv/bin/activate
-<<<<<<< HEAD
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
-=======
-exec python manage.py runserver 0.0.0.0:8000
-# exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
->>>>>>> 2212e8b (minor fix)
