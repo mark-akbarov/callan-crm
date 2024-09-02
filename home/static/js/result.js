3cc575346d60e11c2593b266077a7b57af1426d0
Umarkhon Akbarov <u.akbarov@uic.group>
2024-09-02 20:14:21 +0500

    add static files to ignore

--- home/static/js/result.js
+++
@@ -1,7 +0,0 @@
-const cards = document.querySelectorAll(".grid-item");
-cards.forEach((item) => {
-  item.addEventListener("mouseover", () => {
-    cards.forEach((el) => el.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
